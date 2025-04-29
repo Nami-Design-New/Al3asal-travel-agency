01098c06abbed10ccf196ec5185aeb9c9166d762
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-04-29 23:44:21 +0300

    feat: add container and column structure to Flights route

--- src/routes/Flights.jsx
+++ src/routes/Flights.jsx
@@ -1,7 +1,11 @@
 export default function Flights() {
   return (
     <section className="flights">
-      
+      <div className="container">
+        <div className="col-12 p-2">
+            
+        </div>
+      </div>
     </section>
   )
 }
